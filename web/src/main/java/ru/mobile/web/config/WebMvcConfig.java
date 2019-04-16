93912ddff1bc3f70bf23c7e533f59941fced1e20
OAKutsenko <Ghtlfnjh12>
2019-04-16 19:17:56 +0300

    --

--- web/src/main/java/ru/mobile/web/config/WebMvcConfig.java
+++ web/src/main/java/ru/mobile/web/config/WebMvcConfig.java
@@ -9,11 +9,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/**")
-                .addResourceLocations("/resources/static/");
-
-    }
 }
