4a8fdd1d2f4b4c30763c3b7126631293ea38f43a
OAKutsenko <Ghtlfnjh12>
2019-04-17 18:14:04 +0300

    --

--- mobile/src/main/java/ru/mobile/front/rest/controller/GoodsData.java
+++ mobile/src/main/java/ru/mobile/front/rest/controller/GoodsData.java
@@ -4,14 +4,17 @@ package ru.mobile.front.rest.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.mobile.front.rest.model.UserCreateReq;
 import ru.mobile.front.rest.view.TopicsView;
 import ru.mobile.front.rest.view.UserCreateResp;
 import ru.mobile.lib.rest.exceptions.RestApiException;
+import ru.mobile.lib.rest.exceptions.WebApiException;
 
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.security.Principal;
 import java.util.List;
 
@@ -42,10 +45,11 @@ public class GoodsData {
 
     @RequestMapping(path = "/topic")
     //@PreAuthorize("hasRole('ROLE_USER')")
-    public List<TopicsView> getTopicList(@Pattern(regexp = "\\d{1,3}",message="topic_id.") @RequestParam(name = "topic_id")
-                                                     int topic_id) throws RestApiException {
+    //@RequestBody String operId
+    public ResponseEntity<String> getTopicList(@Size(min = 0, max = 2, message = "txtMsg") @RequestParam(name = "topic_id")
+                                                     String topic_id) throws WebApiException {
 
         log.error("Topic : "+topic_id);
-        return null;
+        return ResponseEntity.ok(""+topic_id);
     }
 }
