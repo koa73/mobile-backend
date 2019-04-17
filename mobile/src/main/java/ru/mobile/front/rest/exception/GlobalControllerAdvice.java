c673b646544740376c10ff5921020627a05f0abf
OAKutsenko <Ghtlfnjh12>
2019-04-17 19:29:30 +0300

    --

--- mobile/src/main/java/ru/mobile/front/rest/exception/GlobalControllerAdvice.java
+++ mobile/src/main/java/ru/mobile/front/rest/exception/GlobalControllerAdvice.java
@@ -81,6 +81,7 @@ public class GlobalControllerAdvice {
 	@ExceptionHandler(Throwable.class)
 	public ResponseEntity<RestApiException> handleException(Throwable throwable, HttpServletRequest req) {
 
+		log.error("----------------------------------------------------------------------------\n");
 		final int cmdCode = Integer.parseInt(req.getRequestURI().replaceAll(".*/(\\d{4}).*?","$1"))+1000;
 
 		log.error("Cause : "+throwable.getCause()+ throwable.getMessage());
