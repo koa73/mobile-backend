8a989cb303f2b559574f3e72e28e24ec01c65a3c
OAKutsenko <Ghtlfnjh12>
2019-04-18 18:16:50 +0300

    --

--- mobile/src/main/java/ru/mobile/front/config/CustomAccessDeniedHandler.java
+++ mobile/src/main/java/ru/mobile/front/config/CustomAccessDeniedHandler.java
@@ -1,6 +1,8 @@
 package ru.mobile.front.config;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,14 @@ import java.io.IOException;
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Override
     public void handle
             (HttpServletRequest request, HttpServletResponse response, AccessDeniedException ex)
             throws IOException, ServletException {
+
+        log.error("----------------------------NNNNNNNNNNNNNNNNNNNNNNNNNNN");
         response.sendError(101, "XXXXX");
     }
 }
