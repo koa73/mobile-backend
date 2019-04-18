e6f351ffd5555e370ae36a6472bb6cb13050096e
OAKutsenko <Ghtlfnjh12>
2019-04-18 18:03:12 +0300

    --

--- mobile/src/main/java/ru/mobile/front/MobileApplication.java
+++ mobile/src/main/java/ru/mobile/front/MobileApplication.java
@@ -83,7 +83,7 @@ public class MobileApplication extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/goods/topic" ).permitAll()
+                .antMatchers("/" ).permitAll()
                 .anyRequest().authenticated();
     }
 }
