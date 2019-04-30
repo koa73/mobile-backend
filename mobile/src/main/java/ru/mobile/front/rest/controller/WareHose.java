bbe6041b5dcde4f26442dea3ced03f40be0b4306
OAKutsenko <Ghtlfnjh12>
2019-04-30 11:18:43 +0300

    --

--- mobile/src/main/java/ru/mobile/front/rest/controller/WareHose.java
+++ mobile/src/main/java/ru/mobile/front/rest/controller/WareHose.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -25,16 +26,14 @@ public class WareHose {
     WarehouseService warehouseService;
 
     @RequestMapping(path = "/goods/items", method = RequestMethod.GET)
-    //@PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<String> getItemList(@IntID(min = 0, max = 3) @RequestParam("topic_id") int id,
                                               Principal principal, HttpServletRequest request) throws RestApiException {
-
-        log.error(request.getAttribute("phone")+" <<<<<--");
         return ResponseEntity.ok(warehouseService.getItems(id));
     }
 
     @RequestMapping(path = "/goods/topic", method = RequestMethod.GET)
-    //@PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<String> getTopicList(@IntID(min = 0, max = 3) @RequestParam("topic_id") int id,
                                                Principal principal) throws RestApiException {
 
